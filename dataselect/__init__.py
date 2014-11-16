8d269c837b0b04762e6e80e462375245f6820250
Alan Du <alanhdu@gmail.com>
2014-11-15 22:41:16 -0500

    Parse into Expression Tree

--- dataselect/__init__.py
+++ dataselect/__init__.py
@@ -1,6 +1,23 @@
 import numpy as np
 import pyparsing as pp
 
+
+class Expr(object):
+    __slots__ = ["head", "args"]
+    def __init__(self, head, args=None):
+        self.head = head
+        self.args = args
+    def __str__(self):
+        if self.args:
+            return str(self.head) + " " + str(self.args)
+        else:
+            return str(self.head)
+    def __repr__(self):
+        return str(self)
+    def __call__(self, data):
+        if self.args is None:
+            return data[self.head]
+
 var = pp.QuotedString('"', "\\")
 digits = pp.Word(pp.nums)
 number = pp.Optional("-") + (digits + "." + digits | digits)
@@ -8,36 +25,35 @@ number = number.setParseAction(lambda t: float("".join(t)))
 operator = pp.oneOf("+ / * - **")
 func_name = pp.Word(pp.alphanums + "_")
 
+
+lpar = pp.Suppress("(")
+rpar = pp.Suppress(")")
+comma = pp.Suppress(",")
+
 expr = pp.Forward()
-func = func_name + "(" + pp.Group(pp.ZeroOrMore(expr + ",") + expr) + ")"
-atom = var | number | "(" + expr + ")" | func
+args = pp.Group(pp.ZeroOrMore(expr + comma) + expr)
+func = (func_name + lpar + args + rpar).setParseAction(lambda t: Expr(t[0], t[1]))
+atom = var | number | lpar + expr + rpar | func
 
-fact = atom + pp.ZeroOrMore("!")
-expo = fact + pp.ZeroOrMore("**" + fact)
-mult = expo + pp.ZeroOrMore(pp.oneOf("* /") + expo)
-addi = mult + pp.ZeroOrMore(pp.oneOf("+ -") + mult)
+def temp(t):
+    if len(t) > 0:
+        e = Expr(t[-1]
+        for fact in t[1:]:
+            e = Expr("!", (e,))
+        return e
+    else:
+        return t
 
-
+fact = (atom + pp.ZeroOrMore("!"))
+def op2Expr(t):
+    if len(t) > 1:
+        e = Expr(t[-1])
+        for i in xrange(len(t) - 2, 0, -2):
+            e = Expr(t[i], (t[i-1], e))
+        return e
+    else:
+        return t
+expo = (fact + pp.ZeroOrMore("**" + fact)).setParseAction(op2Expr)
+mult = (expo + pp.ZeroOrMore(pp.oneOf("* /") + expo)).setParseAction(op2Expr)
+addi = (mult + pp.ZeroOrMore(pp.oneOf("+ -") + mult)).setParseAction(op2Expr)
 expr << addi
-
-"""
-expr = pp.operatorPrecedence(atom,
-            [#("!", 1, pp.opAssoc.LEFT),
-             #("**", 2, pp.opAssoc.RIGHT),
-             #(pp.oneOf("+ -"), 1, pp.opAssoc.RIGHT),
-             #(pp.oneOf("* /"), 2, pp.opAssoc.LEFT),
-             (pp.oneOf("+ -"), 2, pp.opAssoc.LEFT),],
-            lpar="(", rpar=")")
-"""
-
-
-#expr = pp.Forward()
-#expr << atom | expr + operator + expr
-
-"""
-func_name = pp.Word(pp.alphanums + "_")
-expr = pp.Forward()
-args = pp.ZeroOrMore(expr + ",") + expr
-
-expr << infix_expr | atom | func_name + "(" + args + ")"
-"""
