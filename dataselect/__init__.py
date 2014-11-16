2f0589b4da363a11631519c874027f3a5081f7bb
Alan Du <alanhdu@gmail.com>
2014-11-16 01:56:56 -0500

    Feed parsed into SymPy for simplification

--- dataselect/__init__.py
+++ dataselect/__init__.py
@@ -1,59 +1,46 @@
 import numpy as np
 import pyparsing as pp
-
-
-class Expr(object):
-    __slots__ = ["head", "args"]
-    def __init__(self, head, args=None):
-        self.head = head
-        self.args = args
-    def __str__(self):
-        if self.args:
-            return str(self.head) + " " + str(self.args)
-        else:
-            return str(self.head)
-    def __repr__(self):
-        return str(self)
-    def __call__(self, data):
-        if self.args is None:
-            return data[self.head]
-
-var = pp.QuotedString('"', "\\")
-digits = pp.Word(pp.nums)
-number = pp.Optional("-") + (digits + "." + digits | digits)
-number = number.setParseAction(lambda t: float("".join(t)))
-operator = pp.oneOf("+ / * - **")
-func_name = pp.Word(pp.alphanums + "_")
-
+from sympy.parsing.sympy_parser import parse_expr
+import sympy
 
 lpar = pp.Suppress("(")
 rpar = pp.Suppress(")")
 comma = pp.Suppress(",")
+digits = pp.Word(pp.nums)
+number = pp.Optional("-") + (digits + "." + digits | digits)
+number = number.setParseAction(lambda t: float("".join(t)))
+func_name = pp.Word(pp.alphanums + "_")
 
-expr = pp.Forward()
-args = pp.Group(pp.ZeroOrMore(expr + comma) + expr)
-func = (func_name + lpar + args + rpar).setParseAction(lambda t: Expr(t[0], t[1]))
-atom = var | number | lpar + expr + rpar | func
+def parse(s):
+    symbols = {}
+    # hack w/ mutability so inner functions have access
+    symbol_count = {"count": 0} 
 
-def temp(t):
-    if len(t) > 0:
-        e = Expr(t[-1]
-        for fact in t[1:]:
-            e = Expr("!", (e,))
-        return e
-    else:
-        return t
+    expr = pp.Forward()
+    args = pp.Group(pp.ZeroOrMore(expr + comma) + expr)
 
-fact = (atom + pp.ZeroOrMore("!"))
-def op2Expr(t):
-    if len(t) > 1:
-        e = Expr(t[-1])
-        for i in xrange(len(t) - 2, 0, -2):
-            e = Expr(t[i], (t[i-1], e))
-        return e
-    else:
-        return t
-expo = (fact + pp.ZeroOrMore("**" + fact)).setParseAction(op2Expr)
-mult = (expo + pp.ZeroOrMore(pp.oneOf("* /") + expo)).setParseAction(op2Expr)
-addi = (mult + pp.ZeroOrMore(pp.oneOf("+ -") + mult)).setParseAction(op2Expr)
-expr << addi
+    def register_var(t):
+        name = t[0]
+        if name in symbols:
+            return symbols[name]
+        else:
+            symbols[name] = "x{}".format(symbol_count["count"])
+            symbol_count["count"] += 1
+            return symbols[name]
+    var = pp.QuotedString('"', "\\").setParseAction(register_var)
+
+    def register_func(t):
+        func_name, args = t[0], t[1]
+        return "{}({})".format(func_name, str(args)[1:-1])
+    func = (func_name + lpar + args + rpar).setParseAction(register_func)
+    atom = var | number | lpar + expr + rpar | func
+
+    def register_operator(ts):
+        return parse_expr("".join(str(t) for t in ts))
+    fact = atom + pp.ZeroOrMore("!")
+    expo = fact + pp.ZeroOrMore("**" + fact)
+    mult = expo + pp.ZeroOrMore(pp.oneOf("* /") + expo)
+    addi = mult + pp.ZeroOrMore(pp.oneOf("+ -") + mult)
+    expr << addi.setParseAction(register_operator)
+
+    return expr.parseString(s)[0]
