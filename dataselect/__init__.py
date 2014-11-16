f134c3f5eaf7f39f91080de65166be51e4e3e019
Alan Du <alanhdu@gmail.com>
2014-11-16 16:47:46 -0500

    Compute Selector

--- dataselect/__init__.py
+++ dataselect/__init__.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pyparsing as pp
 from sympy.parsing.sympy_parser import parse_expr
+from sympy.core.function import UndefinedFunction
+from toolz import keyfilter
 import sympy
+
+custom_funcs = {"rank": np.log}
+
+def pick(whitelist, d):
+    return keyfilter(lambda k: k in whitelist, d)
 
 lpar = pp.Suppress("(")
 rpar = pp.Suppress(")")
@@ -12,35 +19,72 @@ number = number.setParseAction(lambda t: float("".join(t)))
 func_name = pp.Word(pp.alphanums + "_")
 
 def parse(s):
-    symbols = {}
-    # hack w/ mutability so inner functions have access
-    symbol_count = {"count": 0} 
-
     expr = pp.Forward()
     args = pp.Group(pp.ZeroOrMore(expr + comma) + expr)
 
+    symbols = {}
     def register_var(t):
         name = t[0]
         if name in symbols:
             return symbols[name]
         else:
-            symbols[name] = "x{}".format(symbol_count["count"])
-            symbol_count["count"] += 1
+            symbols[name] = "x{}".format(len(symbols))
             return symbols[name]
     var = pp.QuotedString('"', "\\").setParseAction(register_var)
 
     def register_func(t):
         func_name, args = t[0], t[1]
-        return "{}({})".format(func_name, str(args)[1:-1])
+        return "{}({})".format(func_name, ",".join(map(str, args)))
     func = (func_name + lpar + args + rpar).setParseAction(register_func)
     atom = var | number | lpar + expr + rpar | func
 
-    def register_operator(ts):
-        return parse_expr("".join(str(t) for t in ts))
     fact = atom + pp.ZeroOrMore("!")
     expo = fact + pp.ZeroOrMore("**" + fact)
     mult = expo + pp.ZeroOrMore(pp.oneOf("* /") + expo)
     addi = mult + pp.ZeroOrMore(pp.oneOf("+ -") + mult)
-    expr << addi.setParseAction(register_operator)
+    def f(ts):
+        return "".join(map(str, ts))
+    expr << addi.setParseAction(f)
 
-    return expr.parseString(s)[0]
+    return parse_expr("".join(expr.parseString(s))), symbols
+
+class Selector(object):
+    def __init__(self, s):
+        expr, symbols = parse(s)
+        self.symbols = {v:k for k, v in symbols.iteritems()}
+        self.funcs = {}
+
+        def compute(expr):
+            if isinstance(expr.func, UndefinedFunction):
+
+                name = "f{}".format(len(self.funcs))
+                f = sympy.Symbol(name)
+                self.funcs[name] = ""
+                self.funcs[name] = expr.func(*(compute(arg) for arg in expr.args))
+                return f
+            elif expr.args:
+                args = [compute(arg) for arg in expr.args]
+                return expr.func(*args)
+            else:
+                return expr
+        self.expr = compute(expr)
+    def __call__(self, data):
+        kwargs = {k: data[v] for k, v in self.symbols.iteritems()}
+
+        def compute(expr):
+            symbols = expr.atoms(sympy.Symbol)
+            kwarg = pick(map(str, symbols), kwargs)
+            f = sympy.lambdify(symbols, expr, "numpy")
+            return f(**kwarg)
+
+        for fs in sorted(self.funcs, reverse=True):
+            func = self.funcs[fs]
+            funcname = str(func.func)
+            s = [compute(arg) for arg in func.args]
+            kwargs[fs] = custom_funcs[funcname](*(compute(arg) for arg in func.args))
+
+        return compute(self.expr)
+
+def compute(expr, data):
+    s = Selector(expr)
+    return s(data)
