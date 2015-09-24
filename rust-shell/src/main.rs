6c15e3317242ba463b684d2fe3be72872c5d63dd
Alan Du <alanhdu@gmail.com>
2015-09-24 09:39:37 -0400

    [rust-shell] Implement custom path not using $PATH

--- rust-shell/src/main.rs
+++ rust-shell/src/main.rs
@@ -1,64 +1,79 @@
+use std::collections::VecDeque;
 use std::env;
+use std::fs;
 use std::io::Write;
 use std::io;
 use std::process::Command;
-use std::collections::VecDeque;
 
-fn cd(args: &Vec<&str>) {
-    if args.len() != 1 {
-        println!("Error: cd takes exactly one arg");
-        return;
-    }
-
-    let new = env::set_current_dir(args[0]);
-    if !new.is_ok() {
-        println!("Error: directory not found");
+fn shell_cd(args: &Vec<&str>) {
+    match args.len() {
+        1 => match env::set_current_dir(args[0]) {
+            Err(e) => println!("error: {}", e),
+            Ok(_) => {},
+        },
+        _ => println!("Error: cd takes exactly one arg"),
     }
 }
 
-fn pwd(args: &Vec<&str>) {
+fn shell_pwd(args: &Vec<&str>) {
     match args.len() {
         0 => println!("{}", env::current_dir().unwrap().display()),
         _ => println!("Error: pwd takes no arguments"),
     };
 }
 
-fn path_add(path: &str, dir: &str) {
+fn path_add(path: &mut String, dir: &str) {
     if path.split(":").any(|x| x == dir) {
         return;
-    } else if path.is_empty() {
-        env::set_var("PATH", dir)
-    } else {
-        env::set_var("PATH", path.to_owned() + ":" + dir)
+    } else if !path.is_empty() {
+        path.push(':');
     }
+    path.push_str(dir);
 }
 
-fn path_remove(path: &str, dir: &str) {
+fn path_remove(path: &mut String, dir: &str) {
     // why is to_owned needed here?
     let new_path = path.split(":")
                        .filter(|x| x.to_owned() != dir)
                        .collect::<Vec<&str>>()
                        .join(":");
-    env::set_var("PATH", new_path);
+    path.clear();
+    path.push_str(&new_path);
 }
 
-fn path(args: &Vec<&str>) {
-    match env::var("PATH") {
-        Err(e) => println!("Could not get PATH: {}", e),
-        Ok(path) => match args.len() {
-            0 => println!("{}", path),
-            2 => match args[0] {
-                "+" => path_add(&path, args[1]),
-                "-" => path_remove(&path, args[1]),
-                _ => println!("Error: path usage: path [+/- dir]"),
-            },
-            _ => println!("Error: path usage: path [+/- dir]"),
-        },
+fn find_full_name(command: &str, path: &str) -> String {
+    match command.chars().next() {
+        Some('/') => command.to_owned(),
+        None => command.to_owned(),
+        Some(_) => {
+            for part in path.split(":") {
+                let full = part.to_owned() + "/" + command;
+                if let Ok(file) = fs::metadata(&full) {
+                    if file.is_file() {
+                        return full.to_owned();
+                    }
+                }
+            }
+            command.to_owned()
+        }
     }
 }
 
-fn shell_exec(command: &str, args: &Vec<&str>) {
-    match Command::new(command).args(args).spawn() {
+fn shell_path(args: &Vec<&str>, path: &mut String) {
+    match args.len() {
+        0 => println!("{}", path),
+        2 => match args[0] {
+            "+" => path_add(path, args[1]),
+            "-" => path_remove(path, args[1]),
+            _ => println!("Error: path usage: path [+/- dir]"),
+        },
+        _ => println!("Error: path usage: path [+/- dir]"),
+    }
+}
+
+fn shell_exec(command: &str, args: &Vec<&str>, path: &str) {
+    let full_command = find_full_name(command, path);
+    match Command::new(full_command).args(args).spawn() {
         Err(e) => println!("error: {}", e),
         Ok(mut child) => match child.wait() {
             Err(e) => println!("error: {}", e),
@@ -67,7 +82,7 @@ fn shell_exec(command: &str, args: &Vec<&str>) {
     }
 }
 
-fn shell_history(args: &Vec<&str>, history: &mut VecDeque<String>) {
+fn shell_history(args: &Vec<&str>, history: &mut VecDeque<String>, path: &mut String) {
     match args.len() {
         0 => for (i, s) in history.iter().enumerate() {
             println!("{} {}", i, s);
@@ -80,7 +95,7 @@ fn shell_history(args: &Vec<&str>, history: &mut VecDeque<String>) {
                     if i >= history.len() {
                         println!("Error: arg out of bounds");
                     } else {
-                        eval(&history[i].clone(), history);
+                        eval(&history[i].clone(), history, path);
                     }
                 }
             },
@@ -89,7 +104,7 @@ fn shell_history(args: &Vec<&str>, history: &mut VecDeque<String>) {
     }
 }
 
-fn eval(input: &String, history: &mut VecDeque<String>) -> bool {
+fn eval(input: &str, history: &mut VecDeque<String>, path: &mut String) -> bool {
     let mut args = input.trim().split_whitespace();
 
     if let Some(command) = args.next() {
@@ -100,11 +115,11 @@ fn eval(input: &String, history: &mut VecDeque<String>) -> bool {
         }
         match command {
             "exit" => return true,
-            "pwd" => pwd(&other_args),
-            "cd" => cd(&other_args),
-            "path" => path(&other_args),
-            "history" => shell_history(&other_args, history),
-            _ => shell_exec(command, &other_args),
+            "pwd" => shell_pwd(&other_args),
+            "cd" => shell_cd(&other_args),
+            "path" => shell_path(&other_args, path),
+            "history" => shell_history(&other_args, history, path),
+            _ => shell_exec(command, &other_args, path),
         }
     }
 
@@ -114,6 +129,7 @@ fn eval(input: &String, history: &mut VecDeque<String>) -> bool {
 fn main() {
     env::set_var("PATH", "");
     let mut history: VecDeque<String> = VecDeque::with_capacity(100);
+    let mut path = String::new();
     loop {
         print!("$");
         io::stdout()
@@ -127,7 +143,7 @@ fn main() {
             .ok()
             .expect("Failed to read line");
 
-        if eval(&input, &mut history) {
+        if eval(&input, &mut history, &mut path) {
             break;
         }
     }
