3e65bb864b3adf6485700253bf734e5b14e5c98a
Alan Du <alanhdu@gmail.com>
2015-11-25 22:41:03 -0500

    [python-autodiff] Implement variable substitution

--- python-autodiff/autodiff.py
+++ python-autodiff/autodiff.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 class Expr:
-    def eval(self):
+    def eval(self, **kwargs):
         raise NotImplementedError("eval not implemented")
 
     def __add__(self, other):
@@ -19,26 +19,30 @@ class Expr:
     def __pow__(self, other):
         return Power(self, other)
 
+class Variable(Expr, namedtuple("Variable", ["name"])):
+    def eval(self, **kwargs):
+        return kwargs[self.name]
+
 class Constant(Expr, namedtuple("Constant", ["value"])):
-    def eval(self):
+    def eval(self, **kwargs):
         return self.value
 
 class Add(Expr, namedtuple("Add", ["expr1", "expr2"])):
-    def eval(self):
-        return self.expr1.eval() + self.expr2.eval()
+    def eval(self, **kwargs):
+        return self.expr1.eval(**kwargs) + self.expr2.eval(**kwargs)
 
 class Subtract(Expr, namedtuple("Subtract", ["expr1", "expr2"])):
-    def eval(self):
-        return self.expr1.eval() - self.expr2.eval()
+    def eval(self, **kwargs):
+        return self.expr1.eval(**kwargs) - self.expr2.eval(**kwargs)
 
 class Multiply(Expr, namedtuple("Multiply", ["expr1", "expr2"])):
-    def eval(self):
-        return self.expr1.eval() * self.expr2.eval()
+    def eval(self, **kwargs):
+        return self.expr1.eval(**kwargs) * self.expr2.eval(**kwargs)
 
 class Divide(Expr, namedtuple("Divide", ["expr1", "expr2"])):
-    def eval(self):
-        return self.expr1.eval() / self.expr2.eval()
+    def eval(self, **kwargs):
+        return self.expr1.eval(**kwargs) / self.expr2.eval(**kwargs)
 
 class Power(Expr, namedtuple("Power", ["expr1", "expr2"])):
-    def eval(self):
-        return self.expr1.eval() ** self.expr2.eval()
+    def eval(self, **kwargs):
+        return self.expr1.eval(**kwargs) ** self.expr2.eval(**kwargs)
--- python-autodiff/test_expr.py
+++ python-autodiff/test_expr.py
@@ -1,6 +1,9 @@
+import pytest
+
 import autodiff
 
-def test_constant():
+
+def test_constant_expr():
     x = autodiff.Constant(10)
     y = autodiff.Constant(2)
     assert x.eval() == 10
@@ -15,3 +18,17 @@ def test_constant():
     assert (x - x - x).eval() == -10
     assert (x * x * x).eval() == 1000
     assert (x / x / x).eval() == 0.1
+
+def test_variable_expr():
+    x = autodiff.Variable("x")
+    y = autodiff.Constant(10)
+
+    assert x.eval(x=5) == 5
+    assert (x + y).eval(x=5) == 15
+    assert (x - y).eval(x=5) == -5
+    assert (x * y).eval(x=5) == 50
+    assert (x / y).eval(x=5) == 0.5
+    assert (x ** y).eval(x=2) == 2 ** 10
+
+    with pytest.raises(KeyError):
+        x.eval()
