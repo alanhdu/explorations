a42da168f29a53812c0e71d2639194b6829329e1
Alan Du <alanhdu@gmail.com>
2015-11-25 23:04:58 -0500

    [python-autodiff] Basic forward differentiation

--- python-autodiff/autodiff.py
+++ python-autodiff/autodiff.py
@@ -4,6 +4,9 @@ class Expr:
     def eval(self, **kwargs):
         raise NotImplementedError("eval not implemented")
 
+    def diff(self, **kwargs):
+        raise NotImplementedError("diff not implemented")
+
     def __add__(self, other):
         return Add(self, other)
 
@@ -16,33 +19,57 @@ class Expr:
     def __truediv__(self, other):
         return Divide(self, other)
 
-    def __pow__(self, other):
-        return Power(self, other)
-
 class Variable(Expr, namedtuple("Variable", ["name"])):
     def eval(self, **kwargs):
         return kwargs[self.name]
+
+    def forward_diff(self, **kwargs):
+        if self.name in kwargs:
+            return 1
+        else:
+            return 0
 
 class Constant(Expr, namedtuple("Constant", ["value"])):
     def eval(self, **kwargs):
         return self.value
 
+    def forward_diff(self, **kwargs):
+        return 0
+
 class Add(Expr, namedtuple("Add", ["expr1", "expr2"])):
     def eval(self, **kwargs):
         return self.expr1.eval(**kwargs) + self.expr2.eval(**kwargs)
+
+    def forward_diff(self, **kwargs):
+        return self.expr1.forward_diff(**kwargs) + \
+            self.expr2.forward_diff(**kwargs)
 
 class Subtract(Expr, namedtuple("Subtract", ["expr1", "expr2"])):
     def eval(self, **kwargs):
         return self.expr1.eval(**kwargs) - self.expr2.eval(**kwargs)
 
+    def forward_diff(self, **kwargs):
+        return self.expr1.forward_diff(**kwargs) - \
+            self.expr2.forward_diff(**kwargs)
+
 class Multiply(Expr, namedtuple("Multiply", ["expr1", "expr2"])):
     def eval(self, **kwargs):
         return self.expr1.eval(**kwargs) * self.expr2.eval(**kwargs)
+
+    def forward_diff(self, **kwargs):
+        a = self.expr1.eval(**kwargs) * self.expr2.forward_diff(**kwargs)
+        b = self.expr2.eval(**kwargs) * self.expr1.forward_diff(**kwargs)
+        return a + b
 
 class Divide(Expr, namedtuple("Divide", ["expr1", "expr2"])):
     def eval(self, **kwargs):
         return self.expr1.eval(**kwargs) / self.expr2.eval(**kwargs)
 
-class Power(Expr, namedtuple("Power", ["expr1", "expr2"])):
-    def eval(self, **kwargs):
-        return self.expr1.eval(**kwargs) ** self.expr2.eval(**kwargs)
+    def forward_diff(self, **kwargs):
+        low = self.expr2.eval(**kwargs)
+        dlow = self.expr2.forward_diff(**kwargs)
+        high = self.expr1.eval(**kwargs)
+        dhigh = self.expr1.forward_diff(**kwargs)
+
+        # low d high - high d-low, over denominator squared we go!
+        return (low * dhigh - high * dlow) / low ** 2
--- python-autodiff/test_expr.py
+++ python-autodiff/test_expr.py
@@ -12,7 +12,6 @@ def test_constant_expr():
     assert (x * y).eval() == 20
     assert (x - y).eval() == 8
     assert (x / y).eval() == 5
-    assert (x ** y).eval() == 100
 
     assert (x + x + x).eval() == 30
     assert (x - x - x).eval() == -10
@@ -28,7 +27,30 @@ def test_variable_expr():
     assert (x - y).eval(x=5) == -5
     assert (x * y).eval(x=5) == 50
     assert (x / y).eval(x=5) == 0.5
-    assert (x ** y).eval(x=2) == 2 ** 10
 
     with pytest.raises(KeyError):
         x.eval()
+
+def test_arithmetic_diff():
+    x = autodiff.Variable("x")
+
+    expr = x + x
+    assert expr.forward_diff(x=5) == 2
+    assert expr.forward_diff(x=1) == 2
+
+    expr = (x + autodiff.Constant(10)) * (x + autodiff.Constant(5))
+    # D_x[expr] = D_x[x ** 2 + 15 x + 150] = 2 * x + 15
+    assert expr.forward_diff(x=5) == 25
+    assert expr.forward_diff(x=0) == 15
+
+    expr = (x + autodiff.Constant(10)) / (x * x)
+    # D_x[expr] = D_x[(x + 10) / (x ** 2)] = - 1 / x**2 - 20 / x ** 3
+    assert expr.forward_diff(x=1) == -21
+    assert expr.forward_diff(x=2) == -0.25 - 20 / 8
+
+    expr = x
+    for _ in range(50):
+        expr = expr * x
+    assert expr.forward_diff(x=0) == 0
+    assert expr.forward_diff(x=1) == 51
+    assert expr.forward_diff(x=2) == 51 * 2 ** 50
