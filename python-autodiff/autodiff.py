d95ae401e7f9dffb050757f525b1c36b84e89e8d
Alan Du <alanhdu@gmail.com>
2015-11-26 00:14:51 -0500

    [python-autodiff] Calculate directional derivative

--- python-autodiff/autodiff.py
+++ python-autodiff/autodiff.py
@@ -2,10 +2,10 @@ from collections import namedtuple
 import math
 
 class Expr:
-    def eval(self, **kwargs):
+    def eval(self, point):
         raise NotImplementedError("eval not implemented")
 
-    def diff(self, **kwargs):
+    def diff(self, direction, point):
         raise NotImplementedError("diff not implemented")
 
     def __add__(self, other):
@@ -24,71 +24,71 @@ class Expr:
         return Pow(self, other)
 
 class Variable(Expr, namedtuple("Variable", ["name"])):
-    def eval(self, **kwargs):
-        return kwargs[self.name]
+    def eval(self, point):
+        return point[self.name]
 
-    def forward_diff(self, **kwargs):
-        if self.name in kwargs:
-            return 1
-        else:
-            return 0
+    def forward_diff(self, direction, point):
+        return direction[self.name]
 
 class Constant(Expr, namedtuple("Constant", ["value"])):
-    def eval(self, **kwargs):
+    def eval(self, point):
         return self.value
 
-    def forward_diff(self, **kwargs):
+    def forward_diff(self, direction, point):
         return 0
 
 class Add(Expr, namedtuple("Add", ["expr1", "expr2"])):
-    def eval(self, **kwargs):
-        return self.expr1.eval(**kwargs) + self.expr2.eval(**kwargs)
+    def eval(self, point):
+        return self.expr1.eval(point) + self.expr2.eval(point)
 
-    def forward_diff(self, **kwargs):
-        return self.expr1.forward_diff(**kwargs) + \
-            self.expr2.forward_diff(**kwargs)
+    def forward_diff(self, direction, point):
+        return self.expr1.forward_diff(direction, point) + \
+            self.expr2.forward_diff(direction, point)
 
 class Subtract(Expr, namedtuple("Subtract", ["expr1", "expr2"])):
-    def eval(self, **kwargs):
-        return self.expr1.eval(**kwargs) - self.expr2.eval(**kwargs)
+    def eval(self, point):
+        return self.expr1.eval(point) - self.expr2.eval(point)
 
-    def forward_diff(self, **kwargs):
-        return self.expr1.forward_diff(**kwargs) - \
-            self.expr2.forward_diff(**kwargs)
+    def forward_diff(self, direction, point):
+        return self.expr1.forward_diff(direction, point) - \
+            self.expr2.forward_diff(direction, point)
 
 class Multiply(Expr, namedtuple("Multiply", ["expr1", "expr2"])):
-    def eval(self, **kwargs):
-        return self.expr1.eval(**kwargs) * self.expr2.eval(**kwargs)
+    def eval(self, point):
+        return self.expr1.eval(point) * self.expr2.eval(point)
 
-    def forward_diff(self, **kwargs):
-        a = self.expr1.eval(**kwargs) * self.expr2.forward_diff(**kwargs)
-        b = self.expr2.eval(**kwargs) * self.expr1.forward_diff(**kwargs)
-        return a + b
+    def forward_diff(self, direction, point):
+        x1 = self.expr1.eval(point)
+        x2 = self.expr2.eval(point)
+        d1 = self.expr1.forward_diff(direction, point)
+        d2 = self.expr2.forward_diff(direction, point)
+
+        return x1 * d2 + x2 * d1
 
 class Divide(Expr, namedtuple("Divide", ["expr1", "expr2"])):
-    def eval(self, **kwargs):
-        return self.expr1.eval(**kwargs) / self.expr2.eval(**kwargs)
+    def eval(self, point):
+        return self.expr1.eval(point) / self.expr2.eval(point)
 
-    def forward_diff(self, **kwargs):
-        low = self.expr2.eval(**kwargs)
-        dlow = self.expr2.forward_diff(**kwargs)
-        high = self.expr1.eval(**kwargs)
-        dhigh = self.expr1.forward_diff(**kwargs)
+    def forward_diff(self, direction, point):
+        low = self.expr2.eval(point)
+        dlow = self.expr2.forward_diff(direction, point)
+        high = self.expr1.eval(point)
+        dhigh = self.expr1.forward_diff(direction, point)
 
         # low d high - high d-low, over denominator squared we go!
         return (low * dhigh - high * dlow) / low ** 2
 
 class Pow(Expr, namedtuple("Pow", ["expr1", "expr2"])):
-    def eval(self, **kwargs):
-        return self.expr1.eval(**kwargs) ** self.expr2.eval(**kwargs)
+    def eval(self, point):
+        return self.expr1.eval(point) ** self.expr2.eval(point)
 
-    def forward_diff(self, **kwargs):
+    def forward_diff(self, direction, point):
         # D_x[f ** g] = f ** (g - 1) * (g * f' + f * log f * g'
 
-        base = self.expr1.eval(**kwargs)
-        dbase = self.expr1.forward_diff(**kwargs)
-        exp = self.expr2.eval(**kwargs)
-        dexp = self.expr2.forward_diff(**kwargs)
+        base = self.expr1.eval(point)
+        dbase = self.expr1.forward_diff(direction, point)
+        exp = self.expr2.eval(point)
+        dexp = self.expr2.forward_diff(direction, point)
 
         if base == 0:       # avoid MathDomainError
             return 0
--- python-autodiff/test_expr.py
+++ python-autodiff/test_expr.py
@@ -8,72 +8,76 @@ import autodiff
 def test_constant_expr():
     x = autodiff.Constant(10)
     y = autodiff.Constant(2)
-    assert x.eval() == 10
-    assert y.eval() == 2
-    assert (x + y).eval() == 12
-    assert (x * y).eval() == 20
-    assert (x - y).eval() == 8
-    assert (x / y).eval() == 5
-    assert (x ** y).eval() == 100
+    assert x.eval({}) == 10
+    assert y.eval({}) == 2
+    assert (x + y).eval({}) == 12
+    assert (x * y).eval({}) == 20
+    assert (x - y).eval({}) == 8
+    assert (x / y).eval({}) == 5
+    assert (x ** y).eval({}) == 100
 
-    assert (x + x + x).eval() == 30
-    assert (x - x - x).eval() == -10
-    assert (x * x * x).eval() == 1000
-    assert (x / x / x).eval() == 0.1
+    assert (x + x + x).eval({}) == 30
+    assert (x - x - x).eval({}) == -10
+    assert (x * x * x).eval({}) == 1000
+    assert (x / x / x).eval({}) == 0.1
 
 def test_variable_expr():
     x = autodiff.Variable("x")
     y = autodiff.Constant(10)
 
-    assert x.eval(x=5) == 5
-    assert (x + y).eval(x=5) == 15
-    assert (x - y).eval(x=5) == -5
-    assert (x * y).eval(x=5) == 50
-    assert (x / y).eval(x=5) == 0.5
-    assert (x ** y).eval(x=2) == 2 ** 10
+    assert x.eval(dict(x=5)) == 5
+    assert (x + y).eval(dict(x=5)) == 15
+    assert (x - y).eval(dict(x=5)) == -5
+    assert (x * y).eval(dict(x=5)) == 50
+    assert (x / y).eval(dict(x=5)) == 0.5
+    assert (x ** y).eval(dict(x=2)) == 2 ** 10
 
     with pytest.raises(KeyError):
-        x.eval()
+        x.eval({})
 
 def test_arithmetic_diff():
     x = autodiff.Variable("x")
 
+    direction = {"x": 1}
+
     expr = x + x
-    assert expr.forward_diff(x=5) == 2
-    assert expr.forward_diff(x=1) == 2
+    assert expr.forward_diff(direction, dict(x=5)) == 2
+    assert expr.forward_diff(direction, dict(x=1)) == 2
 
     expr = (x + autodiff.Constant(10)) * (x + autodiff.Constant(5))
     # D_x[expr] = D_x[x ** 2 + 15 x + 150] = 2 * x + 15
-    assert expr.forward_diff(x=5) == 25
-    assert expr.forward_diff(x=0) == 15
+    assert expr.forward_diff(direction, dict(x=5)) == 25
+    assert expr.forward_diff(direction, dict(x=0)) == 15
 
     expr = (x + autodiff.Constant(10)) / (x * x)
     # D_x[expr] = D_x[(x + 10) / (x ** 2)] = - 1 / x**2 - 20 / x ** 3
-    assert expr.forward_diff(x=1) == -21
-    assert expr.forward_diff(x=2) == -0.25 - 20 / 8
+    assert expr.forward_diff(direction, dict(x=1)) == -21
+    assert expr.forward_diff(direction, dict(x=2)) == -0.25 - 20 / 8
 
     expr = x
     for _ in range(50):
         expr = expr * x
-    assert expr.forward_diff(x=0) == 0
-    assert expr.forward_diff(x=1) == 51
-    assert expr.forward_diff(x=2) == 51 * 2 ** 50
+    assert expr.forward_diff(direction, dict(x=0)) == 0
+    assert expr.forward_diff(direction, dict(x=1)) == 51
+    assert expr.forward_diff(direction, dict(x=2)) == 51 * 2 ** 50
 
 def test_power_diff():
     x = autodiff.Variable("x")
 
+    direction = {"x": 1}
+
     expr = x ** autodiff.Constant(2)
-    assert expr.forward_diff(x=0) == 0
-    assert expr.forward_diff(x=1) == 2
-    assert expr.forward_diff(x=2) == 4
+    assert expr.forward_diff(direction, dict(x=0)) == 0
+    assert expr.forward_diff(direction, dict(x=1)) == 2
+    assert expr.forward_diff(direction, dict(x=2)) == 4
 
     expr = autodiff.Constant(math.e) ** x
-    assert expr.forward_diff(x=0) == 1
-    assert expr.forward_diff(x=1) == math.e
-    assert expr.forward_diff(x=2) == math.e ** 2
+    assert expr.forward_diff(direction, dict(x=0)) == 1
+    assert expr.forward_diff(direction, dict(x=1)) == math.e
+    assert expr.forward_diff(direction, dict(x=2)) == math.e ** 2
 
     expr = x
     for i in range(100):
         expr = expr ** x
 
-    assert expr.forward_diff(x=1) == 1
+    assert expr.forward_diff(direction, dict(x=1)) == 1
