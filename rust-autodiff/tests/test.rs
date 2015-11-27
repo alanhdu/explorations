4be3b907683ef03778c6faec3199f5f2a1ad8c1c
Alan Du <alanhdu@gmail.com>
2015-11-26 20:10:03 -0500

    [rust-autodiff] Use quickcheck for tests

--- rust-autodiff/tests/test.rs
+++ rust-autodiff/tests/test.rs
@@ -1,14 +1,14 @@
 extern crate autodiff;
-
+extern crate quickcheck;
 
 #[cfg(test)]
 mod test {
     use autodiff::expr::Expr;
+    use quickcheck::quickcheck;
 
     #[test]
     fn operator_chaining() {
         let a = Expr::constant(10.0);
-
         (&a + &a) + (&a + &a);      // Value + Value, Ref + Ref
         (&a + &a) + &a;             // Value + Ref
         &a + (&a + &a);             // Ref + Value
@@ -25,17 +25,51 @@ mod test {
         (&a / &a) / &a;             // Value / Ref
         &a / (&a / &a);             // Ref / Value
     }
+    #[test]
+    fn constant_add_eval() {
+        fn prop(a: f64, b: f64) -> bool {
+            let x = Expr::constant(a);
+            let y = Expr::constant(b);
+
+            (a + b) == (&x + &y).eval()
+        }
+        quickcheck(prop as fn(f64, f64) -> bool);
+    }
 
     #[test]
-    fn constant_eval() {
-        let a = Expr::constant(10.0);
-        let b = Expr::constant(2.0);
-
-        assert_eq!(a.eval(), 10.0);
-        assert_eq!(b.eval(), 2.0);
-        assert_eq!((&a + &b).eval(), 12.0);
-        assert_eq!((&a + &a + &a).eval(), 30.0);
-        assert_eq!(((&a + &a) + (&a + &a)).eval(), 40.0);
-        assert_eq!((&a - &b).eval(), 8.0);
+    fn constant_sub_eval() {
+        fn prop(a: f64, b: f64) -> bool {
+            let x = Expr::constant(a);
+            let y = Expr::constant(b);
+            (a - b) == (&x - &y).eval()
+        }
+        quickcheck(prop as fn(f64, f64) -> bool);
+    }
+    #[test]
+    fn constant_mul_eval() {
+        fn prop(a: f64, b: f64) -> bool {
+            let x = Expr::constant(a);
+            let y = Expr::constant(b);
+            (a * b) == (&x * &y).eval()
+        }
+        quickcheck(prop as fn(f64, f64) -> bool);
+    }
+    #[test]
+    fn constant_div_eval() {
+        fn prop(a: f64, b: f64) -> bool {
+            let x = Expr::constant(a);
+            let y = Expr::constant(b);
+            (a / b) == (&x / &y).eval()
+        }
+        quickcheck(prop as fn(f64, f64) -> bool);
+    }
+    #[test]
+    fn constant_complicated_eval() {
+        fn prop(a: f64, b: f64) -> bool {
+            let x = Expr::constant(a);
+            let y = Expr::constant(b);
+            (a + b * (a / (a - b))) == (&x + &y * (&x / (&x - &y))).eval()
+        }
+        quickcheck(prop as fn(f64, f64) -> bool);
     }
 }
