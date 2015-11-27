914da253cbe43652d23dd02d43cbbabf72f1142d
Alan Du <alanhdu@gmail.com>
2015-11-27 18:06:31 -0500

    [rust-autodiff] Add documentation

--- rust-autodiff/src/lib.rs
+++ rust-autodiff/src/lib.rs
@@ -3,11 +3,53 @@ pub mod expr {
     use std::ops::{Add, Sub, Mul, Div, BitXor};
     use std::rc::Rc;
 
+    /// User-facing wrapper for `Rc<InnerExpr>`
+    ///
+    /// Use `Rc` because we want shared ownership of immutable `InnerExpr`s.
+    /// That way, we can just `clone` the `Rc` instead of `clone`-ing the entire
+    /// `InnerExpr` enum.
     #[derive(Debug, Clone)]
     pub struct Expr {
         expr: Rc<InnerExpr>
     }
 
+    impl Expr {
+        /// Constructor to represent a constant `Expr`
+        pub fn constant(value: f64) -> Expr {
+            Expr {expr: Rc::new(InnerExpr::Constant(value))}
+        }
+
+        /// Constructor for an `Expr` variable
+        pub fn variable(name: &str) -> Expr {
+            Expr {expr: Rc::new(InnerExpr::Variable(name.to_owned()))}
+        }
+
+        /// Evaluate the `expr` at the point given at `values`.
+        ///
+        /// `values` should map variable name to the value (e.g. {"x" => 1}). We
+        /// don't do any error-handling, so if a variable name is not in
+        /// `values`, we panic. TODO: actual error handling.
+        pub fn eval(&self, values: &HashMap<String, f64>) -> f64 {
+            self.expr.eval(values)
+        }
+
+        /// Evaluate the directional derivative of the `Expr` at the point
+        /// `point` in the direction of `direction`. In other words, it computes
+        /// the dot product of the gradient of Expr @ point with direction.
+        ///
+        /// Both `point` and `direction` should map variable names to their
+        /// values. Again, there's no intelligent error handling if a variable
+        /// is not found.
+        pub fn forward_diff(&self, direction: &HashMap<String, f64>,
+                            point: &HashMap<String, f64>) -> f64 {
+            self.expr.forward_diff(direction, point)
+        }
+    }
+
+    /// Private type that handles the actual heavy-lifting.
+    ///
+    /// We separate Arithmetic out as a separate type to increase modularity. In
+    /// retrospect, this probably wasn't worth the added verbosity.
     #[derive(Debug)]
     enum InnerExpr {
         Arithmetic(Arithmetic),
@@ -15,25 +57,7 @@ pub mod expr {
         Variable(String),
     }
 
-    impl Expr {
-        pub fn constant(value: f64) -> Expr {
-            Expr {expr: Rc::new(InnerExpr::Constant(value))}
-        }
-
-        pub fn variable(name: &str) -> Expr {
-            Expr {expr: Rc::new(InnerExpr::Variable(name.to_owned()))}
-        }
-
-        pub fn eval(&self, values: &HashMap<String, f64>) -> f64 {
-            self.expr.eval(values)
-        }
-
-        pub fn forward_diff(&self, direction: &HashMap<String, f64>,
-                            point: &HashMap<String, f64>) -> f64 {
-            self.expr.forward_diff(direction, point)
-        }
-    }
-
+    /// See documentation for `impl Expr`
     impl InnerExpr {
         fn eval(&self, values: &HashMap<String, f64>) -> f64 {
             match *self {
@@ -53,6 +77,8 @@ pub mod expr {
     }
 
 
+    /// Represents basic arithmetic between expressions. Use reference counted
+    /// pointers for shared ownership of the `InnerExpr`s.
     #[derive(Debug)]
     enum Arithmetic {
         Add(Rc<InnerExpr>, Rc<InnerExpr>),
@@ -99,14 +125,17 @@ pub mod expr {
                     (low * dhigh - high * dlow) / (low.powi(2))
                 },
                 Arithmetic::Pow(ref a, ref b) => {
-                    // D_x(f ** g) = f ** (g - 1) * (gf' + fg' log f)
+                    // D_x[f ** g] = D_x[exp(g ln f)] = exp(g ln f) D_x[g ln f]
+                    //             = exp(g ln f) (gf' / f + g' ln f)
+                    //             = f ** g * (gf' / f + g' ln f)
+                    //             = f ** (g - 1) * (gf' + fg' ln f)
+                    // Verified by WolframAlpha :)
                     let base = a.eval(point);
                     let exp = b.eval(point);
                     let dbase = a.forward_diff(direction, point);
                     let dexp = b.forward_diff(direction, point);
 
-                    // low dhigh - high dlow, over denominator squared we go
-                    if base == 0.0 {
+                    if base == 0.0 {    // avoid divide by zero error
                         0.0
                     } else {
                         base.powf(exp - 1.0) * (exp * dbase + base * dexp * base.ln())
@@ -116,6 +145,35 @@ pub mod expr {
         }
     }
 
+    /// Macro for easy operator overloading.
+    ///
+    /// The only difference between the difference operator overloading are the
+    /// trait names (Add vs Sub), the name of the function (add vs sub), and the
+    /// variant of Arithmetic that we use (Arithmetic::Pow vs Arithmetic::Sub),
+    /// so we abstract the logic into a macro to reduce repetition.
+    ///
+    /// NOTE: Operator overloading in Rust takes things by value, we need to be
+    /// careful to do our arithmetic by references. Unfortunately, because of
+    /// how ownership works, we need to implement the same logic for
+    /// (Value, Value), (Value, Ref), (Ref, Ref), and (Ref, Value). For example:
+    ///     ```
+    ///     let a = Expr::constant(3.0);
+    ///     let b = Expr::constant(4.0);
+    ///
+    ///     let c = a + b;  // ownership of a and b are moved
+    ///     let d = a + b;  // so this fails
+    ///     ```
+    /// Instead, we can do:
+    ///     ```
+    ///     let a = Expr::constant(3.0);
+    ///     let b = Expr::constant(4.0);
+    ///
+    ///     let c = &a + &b;  // Ref + Ref
+    ///     let d = &a + &b;  // Because Add takes references, no ownership problems
+    ///     (&a + &a) + &a;         // (Ref + Ref) + Ref -> Value + Ref
+    ///     &a + (&a + &a);         // Ref + (Ref + Ref) -> Ref + Value
+    ///     (&a + &a) + (&a + &a)   // (Ref + Ref) + (Ref + Ref) -> Value + Value
+    ///     ```
     macro_rules! operator_overload {
         ($trait_name:ident, $struct_name:ident, $func_name:ident, $arith_type:ident) => {
             impl $trait_name<$struct_name> for $struct_name {
